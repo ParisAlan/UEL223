eac7b08e9d28155cbc149d1ebeed8a9ef0ba3f6a
Alan Paris <alanparis2003@gmail.com>
2025-03-11 19:02:58 +0100

    Modification CSS

--- panier.php
+++ panier.php
@@ -1,5 +1,4 @@
 <?php
-	require_once 'modele/modele.php';
-	require_once 'controleur/controleur.php';
-	require_once 'vue/panier.php';
-?>
+require_once 'modele/modele.php';
+require_once 'controleur/controleur.php';
+require_once 'vue/panier.php';
